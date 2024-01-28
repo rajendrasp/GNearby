5c39ffdcaa5d2bad8ca416bee56340519fa17bb1
Rajendra Pardeshi <rajendrasp79@outlook.com>
2024-01-28 13:49:47 +0530

    windows related projects

--- build/internal/platform/implementation/cmake_install.cmake
+++ build/internal/platform/implementation/cmake_install.cmake
@@ -37,3 +37,8 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("C:/D/Work/GNearbyCmake/GNearby/build/internal/platform/implementation/shared/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/D/Work/GNearbyCmake/GNearby/build/internal/platform/implementation/windows/cmake_install.cmake")
+endif()
+
