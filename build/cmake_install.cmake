d399a94b5b32498c6382522101621139307d9521
Rajendra Pardeshi <rajendrasp79@gmail.com>
2024-02-08 22:12:38 +0530

    Added sharing projects

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -157,6 +157,11 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("D:/Code/GNearby/GNearbyCmake/GNearby/build/connections/cmake_install.cmake")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Code/GNearby/GNearbyCmake/GNearby/build/sharing/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
